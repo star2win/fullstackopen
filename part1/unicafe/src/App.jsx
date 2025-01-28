01c39e67394ac1075db5ff73ff87eeb9dd044ebb
Filip Szymanski <fszymanski@gmail.com>
2025-01-28 12:25:46 -0800

    Completed Exerciase 1.7 and fixed button naming from Exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,17 +6,24 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  let all = good + neutral + bad
+  let average = (good - bad) / all
+  let positive = good / all
+
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>good</button>
-      <button onClick={() => setBad(bad + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
       <p>
             good {good}
       <br />neutral {neutral}
       <br />bad {bad}
+      <br />all {all}
+      <br />average {average}
+      <br />positive {positive}
       </p>
     </div>
   )
