9bba692a744f487ab251b64e41467a7be1180550
Filip Szymanski <fszymanski@gmail.com>
2025-02-10 09:35:36 -0800

    Completed Exercise 2.20

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
-const Country = ({country}) => {
+const Country = ({country, weather}) => {
 
-  if (!country) {
+  if (!country || !weather) {
     return
   }
+
+  const weatherIconURL = `https://openweathermap.org/img/wn/${weather.icon}@2x.png`
+  //console.log("weather icon URL", weatherIconURL)
 
   return (
   <div>
@@ -19,23 +22,31 @@ const Country = ({country}) => {
     {Object.keys(country.languages).map(language => <li>{country.languages[language]}</li>)}
     </ul>
     <img src={country.flags.png} />
+    <h3>Weather in {country.capital}</h3>
+    <p>temperature {weather.temp} Celcius</p>
+    <img src={weatherIconURL} />
+    <p>wind {weather.wind} m/s</p>
     {/*
-    <pre>
+     <pre>
       {JSON.stringify(country, null, 2)}
-    </pre>    
+    </pre>   
     */}
   </div>
   )
 }
 
 const App = () => {
-  const [search, setSearch] = useState()
-  const [country, setCountry] = useState()
+  const [search, setSearch] = useState(null)
+  const [country, setCountry] = useState(null)
   const [countries, setCountries] = useState(null)
   const [message, setMessage] = useState(null)
+  const [weather, setWeather] = useState(null)
+
+  const api_key = import.meta.env.VITE_OPENWEATHER_API_KEY
+  //console.log("api key: ", api_key)
 
   useEffect(() => {
-    console.log('country is now', country)
+    //console.log('country is now', country)
 
     if (!countries) {
       axios
@@ -46,7 +57,7 @@ const App = () => {
     }
 
     if (search) {
-      console.log('fetching country...', search)
+      //console.log('fetching country...', search)
       axios
         .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${search}`)
         .then(response => {
@@ -55,14 +66,38 @@ const App = () => {
         .catch(error => {
           console.log('country not found...')
         })
-    }
+      }
+   
   }, [search])
+
+  useEffect(() => {
+    
+    if (country) {
+      const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${country.capitalInfo.latlng[0]}&lon=${country.capitalInfo.latlng[1]}&appid=${api_key}&units=metric`
+      //console.log(url)
+    
+      axios
+        .get(url)
+        .then(response => {
+          //console.log("response", response.data)
+          setWeather({temp: response.data.current.temp, 
+                      wind: response.data.current.wind_speed,
+                      icon: response.data.current.weather[0].icon})
+        })
+        .catch(error => {
+          console.log("Error getting weather: ", error)
+          })
+      }
+
+  }, [country])
+
+
 
   const handleChange = (event) => {
     const searchName = event.target.value
-    console.log(searchName)
+    //console.log(searchName)
     const countriesMatched = countries.filter(c => c.toLowerCase().startsWith(searchName.toLowerCase()))
-    console.log(countriesMatched.length)
+    //console.log(countriesMatched.length)
 
     if (countriesMatched.length > 10) {
       return (
@@ -88,7 +123,7 @@ const App = () => {
         </form>
         {message}
         
-        <Country country={country} />
+        <Country country={country} weather={weather}/>
         {/*
         <br></br>
         Countries List:
